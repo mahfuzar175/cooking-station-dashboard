1e56e522c984669ce1e21501c49227715d1ef319
MD MAHFUZAR RAHMAN TAREK <mahfuzar175@gmail.com>
2024-03-10 21:50:48 +0600

    setup project layout

---
+++ src/Router/router.jsx
@@ -0,0 +1,18 @@
+import {
+    createBrowserRouter,
+  } from "react-router-dom";
+import Main from "../Layout/Main";
+import Dashboard from "../Page/Dashboard";
+
+export const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Main></Main>,
+      children: [
+        {
+            path: '/',
+            element: <Dashboard></Dashboard>
+        }
+      ]
+    },
+  ]);
